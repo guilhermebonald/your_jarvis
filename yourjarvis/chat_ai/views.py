7b74970db211e1b94776d4e71ed48276ee4b2451
Guilherme Bonald <guilhermebonald@hotmail.com>
2023-06-09 19:34:11 -0300

    Add Chat_AI

--- yourjarvis/chat_ai/views.py
+++ yourjarvis/chat_ai/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .service import get_chat
+
 
 # Create your views here.
 def chat_ai(request):
-    return HttpResponse('Chat Ai')
+    get_message = get_chat('Olá Boa Noite!')
+    return HttpResponse(get_message)
