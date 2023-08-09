0b2e4b64efecdfdf08f8856dbf874d9e096dcce7
guilhermebonald <guilhermebonald@hotmail.com>
2023-08-09 12:38:50 -0300

    Rename Classes

--- yourjarvis/jarvis_assist_app/modules/ai_assistant.py
+++ yourjarvis/jarvis_assist_app/modules/ai_assistant.py
@@ -5,34 +5,36 @@ import requests
 class RequestService(ABC):
     """Classe que cria uma interface para o modelo de requisição.
 
-        As classe que herdam essa interface devem implementar o método
-        get_api.
+    As classe que herdam essa interface devem implementar o método
+    get_api.
     """
+
     @abstractmethod
-    def get_api(self, url):
+    def get_api(self, url: str):
         pass
 
 
 class HttpClient(RequestService):
     """Classe que efetuará a requisição.
 
-        Recebe uma "url" do tipo "str".
-        Retorna um tipo "requests.Response".
+    Recebe uma "url" do tipo "str".
+    Retorna um tipo "requests.Response".
     """
-    def get_api(self, url:str) -> requests.Response:
+
+    def get_api(self, url: str) -> requests.Response:
         response = requests.get(url)
         return response
 
 
 class AIAssistant:
-    def __init__(self, api_key, request_method):
+    def __init__(self, api_key, request_service):
         self.api_key = api_key
-        self.request_method = request_method
+        self.request_service = request_service
 
     def get_ai_response(self, message):
         url = f"https://api.betterapi.net/youchat?inputs={message}. Responda em portugues&key={self.api_key}"
 
-        response = self.request_method.get_api(url)
+        response = self.request_service.get_api(url)
         if response.status_code == 200:
             json = response.json()
             return json["generated_text"]
